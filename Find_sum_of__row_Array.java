525a081df7c6ac2adbeaf46273766913d21e6905
SWAPNALI SHIRURE <156194578+Shirureswapnali@users.noreply.github.com>
2025-08-21 00:57:27 +0530

    Add files via upload

---
+++ Find_sum_of__row_Array.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Find_sum_of__row_Array
+{
+public static void main(String args[])
+{
+int row,cols;
+int i,j,sum=0;
+Scanner sc=new Scanner(System.in);
+row=sc.nextInt();
+cols=sc.nextInt();
+int[][]a=new int[row][cols];
+for(i=0;i<row;i++)
+{
+for(j=0;j<cols;j++)
+{
+a[i][j]=sc.nextInt();
+}
+}
+for(i=0;i<row;i++)
+{
+for(j=0;j<cols;j++)
+{
+sum+=a[i][j];
+}
+System.out.println(""+sum);
+}
+}
+}
